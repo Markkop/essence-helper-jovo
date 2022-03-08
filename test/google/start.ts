e08f4a94ed1aa81deccbffbe98c3d638268d3a0f
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-03-08 12:23:49 -0300

    feat: add intent after start test

--- test/google/start.ts
+++ test/google/start.ts
@@ -9,4 +9,13 @@ export function testStart(test: ActionsOnGoogleTestManager, triggerPhrase: strin
     test.assertSpeech(en.translation.FALLBACK_WITH_HELP)
     test.assertConversationNotEnded()
   })
+
+  it('get an intent after not understanding for the first time', async function () {
+    await startConversation(test, triggerPhrase)
+    await test.sendQuery('take me to the moon')
+    test.assertSpeech(en.translation.FALLBACK_WITH_HELP)
+    await test.sendQuery('equipment perk')
+    test.assertSpeech(en.translation.ASK_FOR_PERK)
+    test.assertConversationNotEnded()
+  })
 }
